40ca28e7762d8d03e379ad5cafaa7c5cbc1271cf
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2023-03-06 17:49:00 +0530

    set3 q-8

---
+++ week-2/set-3/q-5.js
@@ -0,0 +1,15 @@
+// Write an ES6 function that takes an array of objects containing student information (name, age, grade) and returns an array with only the students who have a grade above a certain value.
+
+const students = [
+    { name: "Alice", age: 16, grade: 90 },
+    { name: "Bob", age: 17, grade: 80 },
+    { name: "Charlie", age: 15, grade: 95 },
+    { name: "David", age: 16, grade: 85 },
+  ];
+  // Your code here
+  
+  const filterStudentsByGrade = (students, marks) => students.filter((stud) => stud.grade > marks)
+
+  const highGradeStudents = filterStudentsByGrade(students, 85);
+  console.log(highGradeStudents); 
+  // Output: [{ name: "Alice", age: 16, grade: 90 }, { name: "Charlie", age: 15, grade: 95 }]
---
+++ week-2/set-3/q-8.js
@@ -0,0 +1,15 @@
+// Write an ES6 function that takes an array of objects containing game information (title, developer, genre) and returns an array with all the games of a certain genre.
+
+const games = [
+    {title: "The Witcher 3: Wild Hunt", developer: "CD Projekt Red", genre: "RPG"},
+    {title: "Grand Theft Auto V", developer: "Rockstar North", genre: "Action"},
+    {title: "Portal 2", developer: "Valve Corporation", genre: "Puzzle"},
+    {title: "The Legend of Zelda: Breath of the Wild", developer: "Nintendo", genre: "Adventure"}
+  ];
+  // Your code here
+  
+  const filterByGenre = (gamesObj , gen) => gamesObj.filter((game) => game.genre == gen);
+  
+  const filteredGames = filterByGenre(games, "RPG");
+  console.log(filteredGames);
+  // Output: [{title: "The Witcher 3: Wild Hunt", developer: "CD Projekt Red", genre: "RPG"}]
